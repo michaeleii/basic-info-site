91a7ddefb6eb06827ae418e32c22751be87a24ed
Michael Lei <michaelleie0ca@gmail.com>
2023-01-02 17:42:19 -0800

    Remove http js files

--- index.js
+++
@@ -1,5 +0,0 @@
-const handler = require("./handler");
-const http = require("http").createServer(handler);
-const PORT = 8080 || process.env.PORT;
-
-http.listen(PORT, () => console.log(`server is running at ${PORT}`));
